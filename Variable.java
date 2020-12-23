38d4921cc27ceb4daa3f17a6ca49125ad6d5ded3
KrzysztofBojarczuk <69596796+KrzysztofBojarczuk@users.noreply.github.com>
2020-12-23 07:55:11 +0100

    Add files via upload

---
+++ Variable.java
@@ -0,0 +1,9 @@
+public class Variable {
+    public static void main(String[] args)
+    {int value;
+
+    value = 5;
+    System.out.print("Zmienna value jest równa ");
+    System.out.println(value + ".");
+    }
+}
